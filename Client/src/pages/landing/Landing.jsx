e7063cec10c6c8a461c2bc27af5b41e09dcca6bc
Tanmay Gosavi <try.tanmayy@gmail.com>
2026-01-05 00:18:30 +0530

    Add server startup logic in Landing component

--- Client/src/pages/landing/Landing.jsx
+++ Client/src/pages/landing/Landing.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React , {useEffect} from 'react'
 import Header from '../../components/landing/Header.jsx';
 import Hero from '../../components/landing/Hero.jsx';
 import Features from '../../components/landing/Features.jsx';
@@ -6,7 +6,22 @@ import Testinomials from '../../components/landing/Testinomials.jsx';
 import FAQ from '../../components/landing/FAQ.jsx';
 import Footer from '../../components/landing/Footer.jsx';
 import Workflow from '../../components/landing/Workflow.jsx';
+import axios from 'axios';
 const Landing = () => {
+
+  useEffect(() => {
+    const startServer = async ()=>{
+      const serverURL = import.meta.env.VITE_SERVER_URL 
+      try {
+        await axios.get(`${serverURL}/`);
+      } catch (error) {
+        console.error("Error starting server:", error);
+      }
+    }
+    startServer();
+  }, []);
+
+  
   return ( <>
     <Header />
     <main className="relative">
