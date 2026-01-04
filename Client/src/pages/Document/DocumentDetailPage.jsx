aa252bd4c8121d10a982ff93113e00d3a78f0fa0
Tanmay Gosavi <try.tanmayy@gmail.com>
2026-01-05 00:37:35 +0530

    Update API URL environment variable in DocumentDetailPage

--- Client/src/pages/Document/DocumentDetailPage.jsx
+++ Client/src/pages/Document/DocumentDetailPage.jsx
@@ -48,7 +48,7 @@ const DocumentDetailPage = () => {
   }
 
   const baseUrl =
-    import.meta.env.REACT_APP_API_URL ; 
+    import.meta.env.VITE_API_URL ; 
 
   return `${baseUrl}${filePath.startsWith("/") ? "" : "/"}${filePath}`;
 };
